5b69daff38fd8d4edbbacd79a3b4a4cac5226dc6
kraii <matthew.kerry@gmail.com>
2012-06-24 00:29:43 +0100

    Added and implemented some simple test cases

--- src/main/java/romannumeral/RomanNumeralConverter.java
+++ src/main/java/romannumeral/RomanNumeralConverter.java
@@ -1,7 +1,40 @@
 package romannumeral;
 
 public class RomanNumeralConverter {
+
 	public static int convert(String romanNumeral) {
-		return 1;
+		int total = 0;
+		for(int i = 0; i < romanNumeral.length(); i++) { 
+			String character = String.valueOf(romanNumeral.charAt(i));
+			total += convertCharacter(character);
+		}
+		
+		return total;
 	}
+
+	private static int convertCharacter(String romanNumeral) {
+		switch (romanNumeral) {
+		case "I":
+			return 1;
+		case "V":
+			return 5;
+		case "X":
+			return 10;
+		case "L":
+			return 50;
+		case "C":
+			return 100;
+		case "D":
+			return 500;
+		case "M":
+			return 1000;
+		}
+
+		throw new InvalidNumeralException();
+	}
+}
+
+@SuppressWarnings("serial")
+class InvalidNumeralException extends RuntimeException {
+
 }
--- src/test/java/romannumeral/RomanNumeralConverterTest.java
+++ src/test/java/romannumeral/RomanNumeralConverterTest.java
@@ -10,5 +10,44 @@ public class RomanNumeralConverterTest {
 	public void I_gives_1() {
 		assertThat(RomanNumeralConverter.convert("I"), is(1));
 	}
+	
+	@Test
+	public void II_gives_2() {
+		assertThat(RomanNumeralConverter.convert("II"), is(2));
+	}
+	
+	@Test
+	public void V_gives_5() {
+		assertThat(RomanNumeralConverter.convert("V"), is(5));
+	}
+	
+	@Test
+	public void X_gives_10() {
+		assertThat(RomanNumeralConverter.convert("X"), is(10));
+	}
+	
+	@Test
+	public void L_gives_50() {
+		assertThat(RomanNumeralConverter.convert("L"), is(50));
+	}
+	
+	@Test
+	public void C_gives_100() {
+		assertThat(RomanNumeralConverter.convert("C"), is(100));
+	}
+	
+	@Test
+	public void D_gives_500() {
+		assertThat(RomanNumeralConverter.convert("D"), is(500));
+	}
 
+	@Test
+	public void M_gives_1000() {
+		assertThat(RomanNumeralConverter.convert("M"), is(1000));
+	}
+	
+	@Test(expected=InvalidNumeralException.class)
+	public void invalidNumeral_gives_exception() {
+		RomanNumeralConverter.convert("Z");
+	}
 }
